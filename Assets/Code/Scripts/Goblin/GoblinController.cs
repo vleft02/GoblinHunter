ac39541e3330cc1bb96f76589f5d1a463ea0cde1
vleft02 <vleftakis@gmail.com>
2023-12-24 17:18:51 +0200

    File structure changed

---
+++ Assets/Code/Scripts/Goblin/GoblinController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinController : MonoBehaviour, Hittable
+{
+    [SerializeField] private float health = 100;
+    [SerializeField] private bool boss = false;
+
+    public float Health { get => health; set => Health = health; }
+
+    [SerializeField] public float damage = 10f;
+
+    private void Start()
+    {
+        damage = 10f;
+    }
+
+    public void InitEnemy()
+    {
+        health = 200;
+        damage = 3;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        if (health > amount)
+        {
+            health -= amount;
+            PlayerProfile.EnemyDamaged(this.gameObject.name,health);
+        }
+        else
+        {
+            if (boss)
+            {
+                EventManager.BossDefeated();
+            }
+            EventManager.EnemyDeath(this.gameObject);
+            PlayerProfile.EnemyKilled(this.gameObject.name);
+            PlayerProfile.IncrementKills();
+        }
+        Debug.Log("Health After: " + health);
+    }
+
+    public bool HasZeroHealth()
+    {
+        return health == 0;
+    }
+
+}
